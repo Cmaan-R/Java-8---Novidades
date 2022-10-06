176cfa96790d16755bee83b6edb5b092f2545a32
Windows 10 <Windows 10@192.168.200.171>
2022-10-06 14:06:01 -0300

    Update

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -13,41 +10,12 @@ public class OrdenaStrings {
 		palavras.add("Editora casa do código");
 		palavras.add("Caelum");
 
-		Comparator<String> comparador = new ComparadorPorTamanho();
-
 //		Collections.sort(palavras , comparador);
-		palavras.sort(comparador);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
 
-//		for (String p : palavras) {
-//			System.out.println(p);
-//		}
-
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
-
-	}
-
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 2;
-		return 0;
+		palavras.forEach(s -> System.out.println(s));
 	}
 
 }
