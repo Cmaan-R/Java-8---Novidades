454ba93c6031c602ae3d3d79881eadda53eb58eb
Windows 10 <Windows 10@192.168.200.171>
2022-10-06 15:42:58 -0300

    Update

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -10,10 +13,21 @@ public class OrdenaStrings {
 		palavras.add("Editora casa do código");
 		palavras.add("Caelum");
 
-//		Collections.sort(palavras , comparador);
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = String::length;
+
+		Comparator<String> comparator = Comparator.comparing(funcao);
+		palavras.sort(comparator);
 
 		System.out.println(palavras);
+
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(impressor);
 
 		palavras.forEach(s -> System.out.println(s));
 	}
