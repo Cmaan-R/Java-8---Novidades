e6eeaebce10e4be60a82e338b52d82bfb75e04d7
Windows 10 <Windows 10@192.168.200.171>
2022-10-07 11:59:16 -0300

    Update

--- Java8/src/Datas.java
+++ Java8/src/Datas.java
@@ -1,4 +1,6 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -27,6 +29,13 @@ public class Datas {
 		String valorFormatado = proximasOlimpiadas.format(formatador);
 		System.out.println(valorFormatado);
 		
+		DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+		
+		LocalDateTime agora = LocalDateTime.now();
+		System.out.println(agora.format(formatadorComHoras));
+		
+		LocalTime intervalo = LocalTime.of(15, 30);
+		System.out.println(intervalo);
 	}
 
 }
